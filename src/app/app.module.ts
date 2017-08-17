dc15e2d48c72d674b5da5667a55ee2a0f57967d6
Seokwon SONG <softcontext@gmail.com>
2017-08-17 14:59:15 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
+import { ExperimentDetailComponent } from './experiments/experiment-detail/experiment-detail.component';
+
+import { StateService } from './common/state.service';
+import { ExperimentsService } from './common/experiments.service';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'experiments', component: ExperimentsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule, HttpModule, FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperimentsComponent, ExperimentDetailComponent
+  ],
+  providers: [ExperimentsService, StateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
